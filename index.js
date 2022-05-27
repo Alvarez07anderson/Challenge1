89aa7b2c2db14f2240939cd357a003448878dbde
Anderson Alvarez <alvarez07anderson@gmail.com>
2022-05-27 07:40:09 -0700

    Salvando o Arquivo

---
+++ index.js
@@ -0,0 +1,54 @@
+const inputTexto = document.querySelector(".input-texto");
+const mensagem = document.querySelector(".mensagem");
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(inputTexto.value)
+    mensagem.value = textoEncriptado
+    mensagem.style.backgroundImage="none"
+}
+
+function encriptar(stringEncriptada) {
+    let matrizCodigo = [["a", "ap"], ["e", "enter"], ["i", "imes"], ["o", "ober"], ["u", "ufat"]]
+    stringEncriptada = stringEncriptada.toLowerCase(); 
+
+    for(let i=0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+
+    return stringEncriptada;
+
+}
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(inputTexto.value)
+    mensagem.value = textoDesencriptado
+    mensagem.style.backgroundImage="none"
+}
+
+function desencriptar(stringDesencriptada) {
+    let matrizCodigo = [["ap", "a"], ["enter", "e"], ["imes", "i"], ["ober", "o"], ["ufat", "u"]]
+    stringDesencriptada = stringDesencriptada.toLowerCase(); 
+
+    for(let i=0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][0])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1])
+        }
+    }
+
+    return stringDesencriptada;
+
+}
+
+const myText = document.getElementById("mensagem");
+const btnCopy = document.getElementById("btn-copy");
+
+btnCopy.onclick = function () {
+    
+    myText.select();
+
+    document.execCommand("copy");
+
+    myText.value = "";
+};
